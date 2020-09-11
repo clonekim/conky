fdceeb970f6eb2002ccfa33f09858b185570d1a2
daehee.kim <clonekim@gmail.com>
2020-09-11 13:46:42 +0900

    ignore

--- mail.py
+++ mail.py
@@ -1,8 +1,8 @@
 import poplib
 
-mailbox = poplib.POP3('host', 110)
-mailbox.user('user')
-mailbox.pass_('password')
+mailbox = poplib.POP3('smart.whoismail.net', 110)
+mailbox.user('dhkim@aisbiz.co.kr')
+mailbox.pass_('1Qwe5094*')
 
 print(mailbox.stat()[0], 'count')
 mailbox.quit()
